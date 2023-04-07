353b23ff084ca7f605060cf14e9901f28a0c598f
divya <eluminous.se64@gmail.com>
2023-04-07 14:33:15 +0530

    did changes for pdf

--- index.js
+++ index.js
@@ -6,6 +6,12 @@ const fs = require('fs');
 const createCsvWriter  = require('csv-writer').createObjectCsvWriter;
 var cors = require('cors')
 const winston = require('winston');
+const kue = require('kue');
+const queue = kue.createQueue();
+const nodemailer = require('nodemailer');
+const handlebars = require("handlebars");
+
+
 const logger = winston.createLogger({
     level: 'info',
     format: winston.format.json(),
@@ -512,5 +518,275 @@ app.get('/download-image/:imagename', function(req, res) {
     const imagePath = './uploads/profile_photo/'+imagepath;
     res.download(imagePath); // This will download the file
   });
+
+app.get('/generate-pdf',function(req,res){
+    
+    // Create a PDF document and add the data to it it will create pdf and save it to folder
+    const PDFDocument = require('pdfkit');
+    const doc = new PDFDocument();
+    User.find({}).then((data)=>{
+
+        var htmlVar='';
+        htmlVar +='<table>';
+        htmlVar +='<tr>';
+        data.forEach((user) => {
+            
+            htmlVar +='<td>'+user.name+'</td>';  
+            htmlVar +='<td>'+user.email+'</td>';    
+            // doc.fontSize(12).text(user.name);
+        });
+        htmlVar +='</tr>';
+        htmlVar +='</table>';
+        doc.text(`${htmlVar}`);
+        // doc.html('<h1>Hello, world!</h1><p>This is some HTML content in a PDF document.</p>');
+
+        // Save the document to a file
+        doc.pipe(fs.createWriteStream('./pdf/output.pdf'));
+        doc.end();
+    });
+});
+
+
+
+app.get('/generate-pdflib', async (req, res) => {
+    
+  const { PDFDocument,rgb,StandardFonts } = require('pdf-lib');
+  const pdfDoc = await PDFDocument.create();
+  const page = pdfDoc.addPage();
+  const textSize = 24;
+  const text = 'Hello, world!';
+  const { width, height } = page.getSize();
+    // Get a font and set the font size
+    const font = await pdfDoc.embedFont(StandardFonts.Helvetica);
+    const fontSize = 16;
+    // page.drawText('Hello, world', {
+    //     x: 50,
+    //     y: height - 50,
+    //     size: textSize,
+    //     color: rgb(0, 0.53, 0.71),
+    // });
+
+    User.find({}).then((data)=>{ 
+        let y = 700;
+        for (const user of data) { 
+            console.log(text);
+            page.drawText('Hello, world', {
+                 x: 50,
+                 y: height - 50,
+                size: textSize,
+                color: rgb(0, 0.53, 0.71),
+            });
+        }
+    });
+
+  const pdfBytes = await pdfDoc.save();
+  fs.writeFileSync('pdflibpdfs/example.pdf', pdfBytes);
+
+//   res.setHeader('Content-Type', 'application/pdf');
+//   res.setHeader('Content-Disposition', 'attachment; filename=example.pdf');
+//   res.send(pdfBytes);
+});
+
+
+app.get('/generate-html-pdf', async (req, res) => {
+    
+    const pdf = require('html-pdf-node');
+
+    const htmlContent = '<h1>Hello, World!</h1>';
+
+    const options = { 
+        format: 'A4',
+        path: 'htmlpdf/a.pdf',
+        printBackground: true,
+        html: htmlContent
+    };
+  
+    pdf.generatePdf(options).then((res)=> {
+        console.log('PDF generated successfully');
+        // Save the PDF buffer to a file
+        // fs.writeFileSync(options.path, buffer);
+    }).catch(error => {
+        console.error(error.message);
+    });
+    
+});//
+
+//working
+app.get('/generate-htmlpdf', async (req, res) => {
+    console.log('in function of html pdf ...');
+    const pdf = require('html-pdf');
+    const StatichtmlContent = `
+        <html>
+        <head>
+            <title>Test PDF</title>
+        </head>
+        <body>
+            <h1>Test PDF</h1>
+            <p>This is a test PDF generated from HTML content.</p>
+        </body>
+        </html>
+    `;
+
+    var htmlVar='';
+    User.find({}).then((data)=>{   
+        htmlVar +='<table border=1 width="500" height="500" >';
+        htmlVar +='<tr>';
+        htmlVar +='<td><b>Username</b></td>'; 
+        htmlVar +='<td><b>Email</b></td>'; 
+        htmlVar +='<td><b>Profile</b></td>'; 
+        htmlVar +='</tr>';
+        data.forEach((user) => {
+            var profilePath = 'http://localhost:4000';
+            htmlVar +='<tr>';
+            htmlVar +='<td>'+user.name+'</td>';  
+            htmlVar +='<td>'+user.email+'</td>';
+            htmlVar +='<td><img src="'+profilePath+user.profile_photo_path+'" /></td>';    
+            htmlVar +='</tr>';
+        });       
+        htmlVar +='</table>';
+        const options = { 
+            format: 'A4',
+        };    
+        // pdf.create(htmlToSend,options).toFile('./htmlpdf/html.pdf', (err, res) => {
+        //     if (err) {
+        //       console.error(err);
+        //     } else {
+        //       console.log(res);
+        //     }
+        // });
+
+        var profilePath = 'http://localhost:4000';
+        var emailTemplateSource  = fs.readFileSync('./mail.html',"utf8");
+        const template = handlebars.compile(emailTemplateSource, {
+            allowedProtoMethods: {
+              trim: true
+            }
+          });
+   
+        User.find({}).then((data)=>{  
+            
+         
+            var htmlToSend1 = template({
+                data:data, 
+                profilePath:profilePath          
+            });
+            var htmlToSend=template({
+                data:data,
+                profilePath:profilePath
+            });
+            
+            //console.log(htmlToSend);
+            pdf.create(htmlToSend,options).toFile('./htmlpdf/html.pdf', (err, res) => {
+                if (err) {
+                  console.error(err);
+                } else {
+                  console.log(res);
+                }
+            });
+        });
+
+       
+    });
+    
+});//
+
+app.get('/send-bulk-email',function(req,res){
+   
+    const addEmailJob = (email, subject, html) => {
+        console.log('in add queue method..');
+        const job = queue.create('email', {
+        email,
+        subject,
+        html
+        }); 
+
+        job
+        .on('complete', function (){
+          console.log('Job', job.id, 'with name', job.data.email, 'is done');
+        })
+        .on('failed', function (){
+          console.log('Job', job.id, 'with name', job.data.email, 'has failed');
+        });
+
+        job.save((error) => {
+            if (error) {
+            console.error(error);
+            } else {
+            console.log(`Email job added to queue: ${email}`);
+            }
+       });
+    };
+
+    // Add 50,000 email jobs to the queue
+   const emailList = 
+   [
+    { email: 'eluminous.se64@gmail.com', subject: 'Test email 1', html: '<p>This is a test email 1</p>' },
+    { email: 'eluminous_se64@eluminoustechnologies.com', subject: 'Test email 2', html: '<p>This is a test email 2</p>' },    
+    { email: 'eluminous_se86@eluminoustechnologies.com', subject: 'Test email 50000', html: '<p>This is a test email 50000</p>'},
+    { email: 'eluminous_se86@eluminoustechnologies.com', subject: 'Test email 50000', html: '<p>This is a test email 50000</p>'}
+   ];
+    
+    emailList.forEach((emailData) => {
+       addEmailJob(emailData.email, emailData.subject, emailData.html);
+    });
+    
+    queue.process('email', (job, done) => {
+        console.log('innnnnnnnnn queue process function...');
+        //sendEmail(job.data.email, job.data.subject, job.data.html);
+        console.log(job);
+        done();
+   });
+
+
+});//
+
+function sendEmail(email,subject,html)
+{
+    console.log('in send email function');
+
+   var transporter = nodemailer.createTransport({
+        type: "login", // default 
+        sendMail:true,
+        host: 'smtp.gmail.com',//"smtp.ethereal.email",
+        port: 465,
+        secure: true,
+       // service:'gmail', //commented for mailtrap
+        auth: { 
+          user: 'eluminous.se64@gmail.com',
+          pass: 'dtmsaculatsemrqr',
+        },
+        logger: true,
+        debug: true
+      });
+
+      const mailOptions = {
+        from: 'eluminous.se64@gmail.com',
+        to: email,
+        subject,
+        html
+      };
+    
+      transporter.sendMail(mailOptions, (error, info) => {
+        if (error) {
+          console.log(error);
+        } else {
+          console.log(`Email sent to ${email}: ${info.response}`);
+        }
+      });
+
+}//sendEmail function
+
+// 404 middleware
+app.use((req, res, next) => {
+    res.status(404).send({message:'Sorry, we could not find that page!'});
+});  
+
+// Error handling middleware
+app.use((err, req, res, next) => {
+    console.error(err.stack);
+    res.status(500).send('Something went wrong 500 server error!');
+});
+
+
 
 app.listen('4000',console.log('server running on port 4000'));
